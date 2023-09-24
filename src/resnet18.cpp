1d048bc29c868c4f1c998f3e939524c05c6b2dd1
HuPengsheet <1554414201@qq.com>
2023-09-24 13:34:57 +0800

    add annotation

--- src/resnet18.cpp
+++ src/resnet18.cpp
@@ -1,5 +1,4 @@
 #include "net.h"
-
 #include <algorithm>
 #if defined(USE_NCNN_SIMPLEOCV)
 #include "simpleocv.h"
@@ -9,29 +8,36 @@
 #endif
 #include <stdio.h>
 #include <vector>
-#include <iostream>
+
+
 static int detect_resnet18(const cv::Mat& bgr, std::vector<float>& cls_scores)
 {
     ncnn::Net resnet18;
 
     resnet18.opt.use_vulkan_compute = true;
 
-    
+    //分别加载模型的参数和数据
     if (resnet18.load_param("model_param/resnet18.param"))
         exit(-1);
     if (resnet18.load_model("model_param/resnet18.bin"))
         exit(-1);
-
+	//opencv读取图片是BGR格式，我们需要转换为RGB格式
     ncnn::Mat in = ncnn::Mat::from_pixels_resize(bgr.data, ncnn::Mat::PIXEL_BGR2RGB, bgr.cols, bgr.rows, 224, 224);
+    
+    //图像归一标准化，以R通道为例（x/225-0.485）/0.229，化简后可以得到下面的式子
+    //需要注意的式substract_mean_normalize里的方差其实是方差的倒数，这样在算的时候就可以将除法转换为乘法计算
+    //所以norm_vals里用的是1除
     const float mean_vals[3] = {0.485f*255.f, 0.456f*255.f, 0.406f*255.f};
     const float norm_vals[3] = {1/0.229f/255.f, 1/0.224f/255.f, 1/0.225f/255.f};
     in.substract_mean_normalize(mean_vals, norm_vals);
 
     ncnn::Extractor ex = resnet18.create_extractor();
-
+	
+    //把图像数据放入input.1这个blob里
     ex.input("input.1", in);
 
     ncnn::Mat out;
+    //提取出推理结果，推理结果存放在191这个blob里
     ex.extract("191", out);
 
     cls_scores.resize(out.w);
@@ -77,7 +83,8 @@ int main(int argc, char** argv)
     }
 
     const char* imagepath = argv[1];
-
+	
+    //使用opencv读取图片
     cv::Mat m = cv::imread(imagepath, 1);
     if (m.empty())
     {
@@ -87,7 +94,8 @@ int main(int argc, char** argv)
 
     std::vector<float> cls_scores;
     detect_resnet18(m, cls_scores);
-
+	
+    //打印得分前三的类别
     print_topk(cls_scores, 3);
 
     return 0;
